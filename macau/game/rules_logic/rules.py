eddc96592b349ff2f9e79b436ad01ce44bf9c11f
Michał <mifiszon@gmail.com>
2025-01-12 22:45:36 +0100

    rules

--- macau/game/rules_logic/rules.py
+++ macau/game/rules_logic/rules.py
@@ -12,9 +12,9 @@ class Rules:
         def apply_standard_rules(self, deck, player_hand, discard_pile):
             """
             Zasady standardowe Makao:
-            - Można położyć kartę o tym samym kolorze lub tym samym numerze.
-            - Jeśli gracz nie może zagrać żadnej karty, dobiera jedną z talii głównej.
-            - Gra kończy się, gdy gracz nie ma kart na ręce.
+            - Można położyć kartę o tym samym kolorze lub tym samym numerze
+            - Jeśli gracz nie może zagrać żadnej karty, dobiera jedną z talii głównej
+            - Gra kończy się, gdy gracz nie ma kart na ręce
             """
             if not player_hand:
                 return "win"
@@ -27,6 +27,27 @@ class Rules:
                 if deck:
                     player_hand.append(deck.pop())
                 return "draw"
+            
+        def apply_war_rules(self, deck, player_hand, discard_pile)
+            """
+            Zasady Makao bitwa:
+            - Można położyć kartę o tym samym kolorze lub tym samym numerze
+            - Jeśli gracz nie może zagrać żadnej karty, dobiera jedną z talii głównej
+            - Gra kończy się, gdy gracz nie ma kart na ręce
+            - Karta 2 dowolnego koloru - nastepny gracz dobiera 2 karty
+            - Karta 3 dowolnego koloru - nastepny gracz dobiera 3 karty
+            - Karta 4 dowolnego koloru - nastepny gracz daje 4 albo pauzje
+            - Karta Walet dowolnego koloru - gracz żąda karty
+            - Karta Dama karo/pik - bitna
+            - Karta Dama kier/trefl - niebitna
+            - Karta Dama na wszystko, wszystko na dame
+            - Karta Król karo/pik - bitny, nastepny gracz dobiera 5 kart
+            - Karta Król trefl/kier - niebitny, analuje króla bitnego
+            """
+            if not player_hand:
+                return "win"
+            
+            top_card = discard_pile[-1]
 
         def apply_custom_rules(self, deck):
             pass
