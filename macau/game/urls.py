eb4fff8fe27f80bfc6455f3813abb5d2097a2ca6
Michał Ogiba <mifiszon@gmail.com>
2025-01-18 17:35:34 +0100

    img

--- macau/game/urls.py
+++ macau/game/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("rules/", views.rules, name='rules'),
     path("game/", views.game, name='game'),
     path('show_cards/', views.show_cards, name='show_cards'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
