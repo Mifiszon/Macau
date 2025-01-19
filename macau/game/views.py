26e4bf7bf0a21043d164f2943f84a88f8996ec21
Michał <mifiszon@gmail.com>
2025-01-19 20:53:15 +0100

    game

--- macau/game/views.py
+++ macau/game/views.py
@@ -5,7 +5,9 @@ from .rules_logic.rules import Rules
 import random
 
 def deck_generator():
-    """Funkcja generuje talie kart"""
+    """
+    Funkcja generuje talie kart
+    """
     Card.objects.all().delete()
 
     colors = ['pik', 'kier', 'karo', 'trefl']
@@ -21,9 +23,37 @@ def deck_generator():
             image_path = f"cards/{number}_{color}.png"
             Card.objects.create(color=color,marking=None, number=number, image=image_path)
     
+def computer_turn(game):
+    """
+    Ruch kompa
+    """
+    computer_hand = list(game.computer_hand.all())
+    top_card = game.discard_pile.first()
+
+    for card in computer_hand:
+        if can_play(card, top_card):
+            game.computer_hand.remove(card)
+            game.discard_pile.add(card)
+            game.save()
+            return
+    
+    if game.deck.exists():
+        next_card = game.deck.first()
+        game.computer_hand.add(next_card)
+        game.deck.remove(next_card)
+        game.save()
+
+def can_play(card, top_card):
+    """
+    Czy karte mozna zagrac
+    """
+    return card.color == top_card.color or card.number == top_card.number or card.marking == top_card.marking
+    
 
 def home(request):
-    """Strona startowa"""
+    """
+    Strona startowa
+    """
     Player.objects.all().delete()
     form = PlayerForm()
     if request.method == "POST":
@@ -35,7 +65,9 @@ def home(request):
     return render(request, 'home.html', {'form': form})
 
 def rules(request):
-    """Strona z zasadami"""
+    """
+    Strona z zasadami
+    """
     if request.method == "POST":
         selected_rules = request.POST.getlist('rules')
         request.session['selected_rules'] = selected_rules
@@ -43,12 +75,16 @@ def rules(request):
     return render(request, 'rules.html')
 
 def show_cards(request):
-        """Widok do wyświetlania wszystkich kart w grze"""
+        """
+        Wszystkie karty
+        """
         cards = Card.objects.all()
         return render(request, 'show_cards.html', {'cards': cards})
 
 def game(request):
-    """Strona z grą"""
+    """
+    Strona z grą
+    """
     #if not Card.objects.exists():
     #    deck_generator()
     
@@ -68,20 +104,17 @@ def game(request):
     deck = []
 
     if created:
-        # Rozdanie kart dla gracza i komputera
+        # Rozdanie kart
         player_cards = cards[:5]
-        computer_cards = cards[5:10]
-
-        # Pierwsza karta do stosu odrzuconych
-        discard_pile = [cards[10]]
-        deck = cards[11:]
+        computer_cards = cards[5:11]
+        discard_pile = [cards[11]]
+        deck = cards[12:]
 
         # Ustawienie kart w grze
         game.discard_pile.set(discard_pile)
         game.player_hand.set(player_cards)
         game.computer_hand.set(computer_cards)
         game.deck.set(deck)
-
         game.save()
 
     selected_rules = request.session.get('selected_rules', [])
@@ -89,16 +122,41 @@ def game(request):
     
     result = rules.apply_rules(cards, player_cards, discard_pile)
 
+    if request.method == "POST":
+        if "play_card" in request.POST:
+            card_id = request.POST.get("card_id")
+            if card_id:
+                card = Card.objects.get.all(id=card_id)
+                if card in game.player_hand.all():
+                    top_card = game.discard_pile.first()
+                    if can_play(card, top_card):
+                        game.player_hand.remove(card)
+                        game.discard_pile.add(card)
+                        game.save()
+                    else:
+                        return render(request, 'game.html', {
+                            'game': game,
+                            'error' : 'Nie możesz zagrać tej karty'
+                        })
+                    
+        elif "draw_card" in request.POST:
+            if game.deck.exists():
+                next_card = game.deck.first()
+                game.player_hand.add(next_card)
+                game.deck.remove(next_card)
+                game.save()
+
+    computer_turn(game)
+
     if result == "Win":
-        return render(request, 'game/win.html', {'player': player})
+        return render(request, 'game/win.html', {'player': game.player})
     elif result == "Lose":
-        return render(request, 'game/lose.html', {'player': player})
+        return render(request, 'game/lose.html', {'player': game.player})
+    
+    top_card = game.discard_pile.first() if game.discard_pile.exists() else None
 
     return render(request, 'game.html', {
         'game': game,
-        'player_cards': player_cards,
-        'computer_cards': computer_cards,
-        'deck': deck,
-        'discard_pile': discard_pile[0] if discard_pile else None,  # Sprawdzamy, czy lista jest pusta
+        'top_card': top_card,
     })
 
