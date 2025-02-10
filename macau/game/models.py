4e0bc553ae2a9f37040a75aa38c15fa3b4417a90
Michał Ogiba <mifiszon@gmail.com>
2025-02-10 22:29:55 +0100

    multi

--- macau/game/models.py
+++ macau/game/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 class Card(models.Model):
     COLORS = [('pik', 'Pik'), ('kier', 'Kier'), ('karo', 'Karo'), ('trefl', 'Trefl')]
@@ -12,32 +13,54 @@ class Card(models.Model):
     order = models.IntegerField(default=0)
 
     def __str__(self):
-        if self.marking:
-            return f"{self.marking} {self.color}"
-        else:
-            return f"{self.number} {self.color}"
-        
+        return f"{self.marking if self.marking else self.number} {self.color}"
+
 class Player(models.Model):
     nick = models.CharField(max_length=20, unique=True)
-    
+
     def __str__(self):
         return self.nick
-    
-class Game(models.Model):
-    player = models.OneToOneField(Player, on_delete=models.CASCADE, related_name="game_player")
-    opponent = models.OneToOneField(Player, on_delete=models.CASCADE, related_name="game_opponent", null=True, blank=True)
-    computer_hand = models.ManyToManyField(Card, related_name="computer_hand")
-    deck = models.ManyToManyField(Card, related_name="deck")
-    rules = models.CharField(max_length=50, null=True, blank=True)
-    opponent_hand = models.ManyToManyField(Card, related_name='opponent_cards', blank=True)
-    turn_action_done = models.BooleanField(default=False)
-    last_played_card = models.ForeignKey(Card, on_delete=models.SET_NULL, null=True, related_name="last_played")
-    player_hand = models.ManyToManyField(Card, related_name="player_hand")
-    discard_pile = models.ManyToManyField(Card, related_name='discard_pile')
-    turn = models.CharField(max_length=10, choices=[('player', 'Gracz'), ('opponent', 'Przeciwnik')], default='player')
-                                         
-    def __str__(self):
-        return f"Game with {self.player.nick}"
-    
+
 class Room(models.Model):
-    pass
+    code = models.CharField(max_length=10, unique=True)
+    players = models.ManyToManyField(Player, related_name="rooms")
+    max_players = models.IntegerField(default=4)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Room {self.code}"
+
+class Game(models.Model):
+    room = models.OneToOneField(Room, on_delete=models.CASCADE, related_name="game")
+    players = models.ManyToManyField(Player, related_name="games")
+    deck = models.ManyToManyField(Card, related_name="deck")
+    discard_pile = models.ManyToManyField(Card, related_name='discard_pile')
+    hands = models.JSONField(default=dict)  # Przechowuje karty w rękach graczy
+    turn_order = models.JSONField(default=list)  # Kolejność tur graczy
+    current_turn = models.IntegerField(default=0)  # Indeks gracza, który ma turę
+    turn_action_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Game in {self.room.code}"
+    
+    def next_turn(self):
+        self.current_turn = (self.current_turn + 1) % len(self.turn_order)
+        self.turn_action_done = False
+        self.save()
+    
+    def start_game(self):
+        """Rozpoczyna grę, tasuje karty i rozdaje po 5 dla każdego gracza."""
+        cards = list(Card.objects.all())
+        random.shuffle(cards)
+        
+        self.deck.set(cards[10:])
+        self.discard_pile.set([cards[9]])
+        
+        player_hands = {}
+        for i, player in enumerate(self.players.all()):
+            player_hands[player.nick] = [card.id for card in cards[i * 5:(i + 1) * 5]]
+        
+        self.hands = player_hands
+        self.turn_order = [player.nick for player in self.players.all()]
+        self.current_turn = 0
+        self.save()
